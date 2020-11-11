1ca064524db6491a8968fe6f493fc53a11fc896e
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-11 18:22:09 -0300

    feat: added route to download the file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
-const printController = require('../controller/print-controller')
+const printController = require('../controllers/print-controller')
 
 router.post('/print', printController.print)
+
+router.get('/screenshot/:id', printController.download)
 
 module.exports = router
